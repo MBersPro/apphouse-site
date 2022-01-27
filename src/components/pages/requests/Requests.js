2ccecf6e11fa61d587317d2edb72f6033befaed6
MBers <mbers.pro@gmail.com>
2022-01-27 03:46:40 +0300

    Done

--- src/components/pages/requests/Requests.js
+++ src/components/pages/requests/Requests.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Requests.module.css";
-const Requests = ({ requests }) => {
+const Requests = ({ requests, questions }) => {
   return (
     <section className={styles.requests_background}>
       <div className={styles.requests_container}>
@@ -8,21 +8,28 @@ const Requests = ({ requests }) => {
           <h4 className={styles.requests_h4}>Запросы:</h4>
           <ul className={styles.requests_Rul}>
             {requests.map((item) => (
-              <li className={styles.requests_Rli}>
-                <p className={styles.requests_Rp}>
+              <li className={styles.requests_li}>
+                <p className={styles.requests_p}>
                   <span className={styles.requests_Rname}>{item.name}</span>{" "}
                   делает запрос "{item.comment}"
                 </p>
-                <p className={styles.requests_Rp}>
+                <p className={styles.requests_p}>
                   Номер клиента: {item.phone}
                 </p>
-                <p className={styles.requests_Rp}>Почта клиента: {item.mail}</p>
+                <p className={styles.requests_p}>Почта клиента: {item.mail}</p>
               </li>
             ))}
           </ul>
         </div>
         <div className={styles.requests_Qcontainer}>
           <h4 className={styles.requests_h4}>Вопросы:</h4>
+          <ul>
+            {questions.map((question) => (
+              <li className={styles.requests_li}>
+                <p className={styles.requests_p}>{question}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
